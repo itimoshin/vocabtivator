347a195bd20441b2bc2df1caffb64aca58f55b02
Ivan Timoshin <pt.itimoshin@prosper.com>
2020-06-07 18:31:49 +0300

    frontend: show hints keyboard shortcuts

--- frontend/src/practice/hint/HintsListComponent.tsx
+++ frontend/src/practice/hint/HintsListComponent.tsx
@@ -5,8 +5,26 @@ import {bindActionCreators} from "redux";
 import {Hint} from "../../model/models";
 import HintComponent from "./HintComponent";
 import './HintsListComponent.scss'
+import {ShowHintActionData} from "../../redux/action-data/hint-actions-data";
 
-const HintsListComponent  = (props: {hints: Hint[], [key: string]: any}) => {
+const HintsListComponent = (props: { hints: Hint[], [key: string]: any }) => {
+
+    function handleKeyDown(e: KeyboardEvent) {
+        if (e.shiftKey && /Digit\d/.test(e.code)) {
+            const idx = Number(e.code[e.code.length - 1]) - 1;
+            if (idx <= props.hints.length - 1 && idx >= 0) {
+                props.actions.genericAction(new ShowHintActionData(props.hints[idx]))
+            }
+        }
+    }
+
+    React.useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [props.hints]);
+
     return (
         <div className="vocabtivator_hints">
             {props.hints.map((h, i) => <HintComponent key={i} hint={h}/>)}
